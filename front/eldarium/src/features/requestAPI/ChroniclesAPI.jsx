6f4796c6627c22b4d19d4ebc549c590fcd83c380
Joris Monleon <monleon.joris@gmail.com>
2021-10-26 16:10:34 +0200

    Add call en fonction de box

--- front/eldarium/src/features/requestAPI/ChroniclesAPI.jsx
+++ front/eldarium/src/features/requestAPI/ChroniclesAPI.jsx
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
+import { useState } from 'react'
 
 export function ChroniclesAPI() {
-  const Id = "2";
   const URL = 'http://localhost:5000/chronicle/';
+  const [box, updateBox] = useState(0)
 
   useEffect(() => {
     const chroniclesRequest = async () => {
       try {
-        const response = await fetch(URL + Id);
+        const response = await fetch(URL + box);
         const jsonRes = await response.json();
         console.log(jsonRes);
       } catch {
@@ -15,9 +16,14 @@ export function ChroniclesAPI() {
       }
     }
     chroniclesRequest();
-  }, []);
+  }, [box]);
 
   return (
+    <div>
       <div>Chronicles Request</div>
+      <button onClick={() => updateBox(box + 1)}> Chapter + 1 </button>
+      <button onClick={() => updateBox(box - 1)}> Chapter - 1 </button>
+      <div>le chapitre: {box}</div>
+    </div>
   ) ;
 }
